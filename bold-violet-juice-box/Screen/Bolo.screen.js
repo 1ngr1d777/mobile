bfc68900698b9f293df7c647ad0222d50ac535ab
Ingrid Beatriz <araujo.silva.ingrid@escola.pr.gov.br>
2025-07-03 11:46:14 -0300

    Add files via upload

---
+++ bold-violet-juice-box/Screen/Bolo.screen.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<Stack.Screan name="Bolo" component=
+{BoloScrean} opitons=
+{{title: 'Bolo de chocolate'}}/>
+
+
+<Stack.Screan name="pão" component=
+{paoScrean} opiton
+'pao caseiro'}}
+/>
